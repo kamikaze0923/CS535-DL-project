9b7f5f19378b909f3d92b934a686ecfbd5d3d148
cyclone923 <32120469+cyclone923@users.noreply.github.com>
2018-03-03 19:55:59 -0800

    Delete txt_to_csv.py

--- txt_to_csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-txt_file = r"./Data/ETFs/aadr.us.txt"
-csv_file = r"./mycsv.csv"
-
-in_txt = csv.reader(open(txt_file, "r"), delimiter = ',')
-out_csv = csv.writer(open(csv_file, 'w'))
-
-out_csv.writerows(in_txt)
