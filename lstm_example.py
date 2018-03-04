7a1461fa2b99bc6890f8c9b1d4045f026a3d4007
Chengxi <yangchen@oregonstate.edu>
2018-03-03 23:18:49 -0800

    change draw

--- lstm_example.py
+++ lstm_example.py
@@ -40,7 +40,9 @@ class Sequence(nn.Module):
         outputs = torch.stack(outputs, 1).squeeze(2)
         return outputs
 
-
+def draw(yi, color):
+    plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
+    plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
 
 np.random.seed(2)
 
@@ -87,21 +89,20 @@ for i in range(15):
     print('test loss:', loss.data)
     y = pred.data.cpu().numpy()
 
+    # draw the result
+    plt.figure(figsize=(30, 10))
+    plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
+    plt.xlabel('x', fontsize=20)
+    plt.ylabel('y', fontsize=20)
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    draw(y[0], 'r')
+    draw(y[1], 'g')
+    draw(y[2], 'b')
+    plt.show()
 
-def draw(yi, color):
-    plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-    plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
 
 
 
-# draw the result
-plt.figure(figsize=(30,10))
-plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-plt.xlabel('x', fontsize=20)
-plt.ylabel('y', fontsize=20)
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
-draw(y[0], 'r')
-draw(y[1], 'g')
-draw(y[2], 'b')
-plt.show()
+
+
