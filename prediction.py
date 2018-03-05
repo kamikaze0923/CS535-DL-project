6af2007249f6bac017fba1c2c5529e94391c4059
Chengxi <yangchen@oregonstate.edu>
2018-03-04 22:29:11 -0800

    change

--- prediction.py
+++
@@ -1,9 +0,0 @@
-import csv
-import numpy
-
-
-with open('cropped_prices.csv', 'r') as f:
-    reader = csv.reader(f)
-    head_strings = next(reader)
-    for i, line in enumerate(reader):
-        date, symbol, open, close, high, low, volumn = tuple(line)
---
+++ prediction_ycx.py
@@ -0,0 +1,6 @@
+import csv
+import numpy as np
+
+
+data = np.genfromtxt('./data/data.csv',delimiter=',',dtype='float32')
+a = 3
