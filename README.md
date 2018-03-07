9b281664d57c45ba8f4a122933c8c2c7e2308566
Chengxi <yangchen@oregonstate.edu>
2018-03-06 17:31:03 -0800

    change

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # CS535-DL-project
+
+result1:
+
+lstm1:1*100
+lstm2:100*100
+linear:100*1
+
+train_feature: 250:2415
+time: 600:1008
+
--- prediction_ycx.py
+++ prediction_ycx.py
@@ -13,16 +13,16 @@ import torch.optim as optim
 class Sequence(nn.Module):
     def __init__(self):
         super(Sequence, self).__init__()
-        self.lstm1 = nn.LSTMCell(1, 50)
-        self.lstm2 = nn.LSTMCell(50, 50)
-        self.linear = nn.Linear(50, 1)
+        self.lstm1 = nn.LSTMCell(1, 100)
+        self.lstm2 = nn.LSTMCell(100, 100)
+        self.linear = nn.Linear(100, 1)
 
     def forward(self, input, future = 0):
         outputs = []
-        h_t = Variable(torch.zeros(input.size(0), 50), requires_grad=False).cuda()
-        c_t = Variable(torch.zeros(input.size(0), 50), requires_grad=False).cuda()
-        h_t2 = Variable(torch.zeros(input.size(0), 50), requires_grad=False).cuda()
-        c_t2 = Variable(torch.zeros(input.size(0), 50), requires_grad=False).cuda()
+        h_t = Variable(torch.zeros(input.size(0), 100), requires_grad=False).cuda()
+        c_t = Variable(torch.zeros(input.size(0), 100), requires_grad=False).cuda()
+        h_t2 = Variable(torch.zeros(input.size(0), 100), requires_grad=False).cuda()
+        c_t2 = Variable(torch.zeros(input.size(0), 100), requires_grad=False).cuda()
 
         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
@@ -45,13 +45,13 @@ mean = np.mean(data,axis=0)
 std = np.std(data,axis=0)
 data_nor = (data - mean)/std
 data_nor = data_nor.transpose()
-data_nor = data_nor[:1500,:]
+data_nor = data_nor[:,600:]
 
-
-input = Variable(torch.from_numpy(data_nor[5:, :-8]), requires_grad=False).cuda()
-target = Variable(torch.from_numpy(data_nor[5:, 1:-7]), requires_grad=False).cuda()
-test_input = Variable(torch.from_numpy(data_nor[:5, :-8]), requires_grad=False).cuda()
-test_target = Variable(torch.from_numpy(data_nor[:5, 1:-7]), requires_grad=False).cuda()
+future = 50
+input = Variable(torch.from_numpy(data_nor[250:, :-future]), requires_grad=False).cuda()
+target = Variable(torch.from_numpy(data_nor[250:, 1:-future + 1]), requires_grad=False).cuda()
+test_input = Variable(torch.from_numpy(data_nor[:250, :-future]), requires_grad=False).cuda()
+test_target = Variable(torch.from_numpy(data_nor[:250, 1:-future + 1]), requires_grad=False).cuda()
 
 
 
@@ -59,10 +59,20 @@ test_target = Variable(torch.from_numpy(data_nor[:5, 1:-7]), requires_grad=False
 seq = Sequence().cuda()
 criterion = nn.MSELoss()
 
+pretrained_dict = torch.load('mytraining.pth')
+model_dict = seq.state_dict()
+for i in model_dict:
+    if i in pretrained_dict:
+        model_dict[i] = pretrained_dict[i]
+seq.load_state_dict(model_dict)
+
+
 # use LBFGS as optimizer since we can load the whole data to train
-optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
+optimizer = optim.LBFGS(seq.parameters(), lr=0.1)
+
+
 #begin to train
-for i in range(3):
+for i in range(20):
     print('STEP: ', i)
     def closure():
         optimizer.zero_grad()
@@ -73,11 +83,23 @@ for i in range(3):
         return loss
     optimizer.step(closure)
     # begin to predict
-    future = 8
     pred = seq(test_input, future = future)
     loss = criterion(pred[:, :-future], test_target)
     print('test loss:', loss.data)
     y = pred.data.cpu().numpy()
 
 
-np.savetxt("result_ycx.csv", y, delimiter=",")
+
+torch.save(seq.state_dict(), 'mytraining.pth')
+for i in range(future):
+    input = Variable(torch.from_numpy(data_nor[:250, :-future+i]), requires_grad=False).cuda()
+    pred = seq(input, future = 1)
+    y = pred.data.cpu().numpy()
+    l = std.size
+    if i == 0:
+        pre = y[:,-1:]
+    else:
+        pre = np.concatenate((pre,y[:,-1:]),axis=1)
+
+np.savetxt("result_ycx.csv", pre, delimiter=",")
+
