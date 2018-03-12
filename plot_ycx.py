3054e1336c832c1389a4928d475d36180acbbca8
Chengxi <yangchen@oregonstate.edu>
2018-03-12 16:19:14 -0700

    change

--- plot_ycx.py
+++ plot_ycx.py
@@ -1,36 +1,39 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-future = 50
+future = 300
 
 pre = np.genfromtxt('./result_ycx.csv',delimiter=',',dtype='float32')
 pre = pre[:,-future:]
 
 
-data = np.genfromtxt('./data/data.csv',delimiter=',',dtype='float32',usecols=tuple([i for i in range(250)]))
+
+data = np.genfromtxt('./data/data.csv',delimiter=',',dtype='float32',usecols=[i*5+1 for i in range(483)])
 act = data.transpose()
 act = act[:,-future:]
 
 
-def draw(pre, act, color, label1, label2):
-    plt.plot(np.arange(future), act, color=color, linewidth=1, label=label1)
-    plt.plot(np.arange(future), pre, color=color, linestyle=':', linewidth=1, label=label2)
+
+
+def draw(pre, act, label1, label2):
+    plt.plot(np.arange(future), act, color='r', linewidth=1, label=label1)
     plt.scatter(np.arange(future), act, marker='+')
+    plt.plot(np.arange(future), pre, color='b', linestyle=':', linewidth=1, label=label2)
     plt.scatter(np.arange(future), pre, marker='*')
 
-
+n = 1
 # draw the result
 plt.figure(figsize=(30, 10))
-plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
+plt.title('Stock %s Last 300 days prediction\n' % str(n+1), fontsize=30)
 plt.xlabel('x', fontsize=20)
 plt.ylabel('y', fontsize=20)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
-draw(pre[6], act[6], 'blue', 'act_open', 'pre_open')
-# draw(pre[1], act[1], 'orange', 'act_close', 'pre_close')
-# draw(pre[2], act[2], 'green', 'act_low', 'pre_low')
-# draw(pre[3], act[3], 'red', 'act_high', 'pre_high')
-# draw(pre[4], act[4], 'purple', 'volume')
+
+
+
+draw(pre[n], act[n], 'Actual', 'Predicted')
+
 legend = plt.legend(loc='0')
 plt.grid(linewidth=1)
 plt.show()
--- prediction_ycx.py
+++ prediction_ycx.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.optim as optim
-
+import torch.nn.functional as F
 
 
 
@@ -28,11 +28,13 @@ class Sequence(nn.Module):
             h_t, c_t = self.lstm1(input_t, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
+
             outputs += [output]
         for i in range(future):# if we should predict the future
             h_t, c_t = self.lstm1(output, (h_t, c_t))
             h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
+
             outputs += [output]
         outputs = torch.stack(outputs, 1).squeeze(2)
         return outputs
@@ -40,18 +42,19 @@ class Sequence(nn.Module):
 
 
 
-data = np.genfromtxt('./data/data.csv',delimiter=',',dtype='float32')
+data = np.genfromtxt('./data/data.csv',delimiter=',',dtype='float32',usecols=[i*5+1 for i in range(483)])
 mean = np.mean(data,axis=0)
 std = np.std(data,axis=0)
 data_nor = (data - mean)/std
 data_nor = data_nor.transpose()
-data_nor = data_nor[:,600:]
 
-future = 50
-input = Variable(torch.from_numpy(data_nor[250:, :-future]), requires_grad=False).cuda()
-target = Variable(torch.from_numpy(data_nor[250:, 1:-future + 1]), requires_grad=False).cuda()
-test_input = Variable(torch.from_numpy(data_nor[:250, :-future]), requires_grad=False).cuda()
-test_target = Variable(torch.from_numpy(data_nor[:250, 1:-future + 1]), requires_grad=False).cuda()
+future = 300
+test_num = 13
+input = Variable(torch.from_numpy(data_nor[test_num:, :-future]), requires_grad=False).cuda()
+target = Variable(torch.from_numpy(data_nor[test_num:, 1:-future + 1]), requires_grad=False).cuda()
+
+test_input = Variable(torch.from_numpy(data_nor[:test_num, :-future]), requires_grad=False).cuda()
+test_target = Variable(torch.from_numpy(data_nor[:test_num, 1:-future + 1]), requires_grad=False).cuda()
 
 
 
@@ -72,7 +75,7 @@ optimizer = optim.LBFGS(seq.parameters(), lr=0.1)
 
 
 #begin to train
-for i in range(1):
+for i in range(2):
     print('STEP: ', i)
     def closure():
         optimizer.zero_grad()
@@ -92,14 +95,15 @@ for i in range(1):
 
 torch.save(seq.state_dict(), 'mytraining.pth')
 for i in range(future):
-    input = Variable(torch.from_numpy(data_nor[:250, :-future+i]), requires_grad=False).cuda()
-    pred = seq(input, future = 1)
+    input = Variable(torch.from_numpy(data_nor[:test_num, i:-future+i]), requires_grad=False).cuda()
+    ahead = 10
+    pred = seq(input, future = ahead)
     y = pred.data.cpu().numpy()
     l = std.size
     if i == 0:
-        pre = y[:,-1:] * std[:250].reshape(250,1) + mean[:250].reshape(250,1)
-    else:
-        pre = np.concatenate((pre,y[:,-1:]* std[:250].reshape(250,1) + mean[:250].reshape(250,1)),axis=1)
+        pre = y[:,-ahead:] * std[:test_num].reshape(test_num,1) + mean[:test_num].reshape(test_num,1)
+    elif i%ahead == 0:
+        pre = np.concatenate((pre,y[:,-ahead:]* std[:test_num].reshape(test_num,1) + mean[:test_num].reshape(test_num,1)),axis=1)
 
 np.savetxt("result_ycx.csv", pre, delimiter=",")
 
